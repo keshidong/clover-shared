20293573c533eea2d8d252137ccaca8742a24799
shidong.ke <shidong.ke@shopee.com>
2020-08-19 15:59:20 +0800

    feat: Add react module: EventShell

---
+++ packages/react-shared/src/EventShell.ts
@@ -0,0 +1,34 @@
+
+import React, { useEffect, useRef } from 'react'
+import ReactDOM from 'react-dom'
+
+const EventShell = function ({ children, addEventListener }) {
+    const refs: any[] = [];
+    const _children = React.Children.map(children, (child, index: number) => {
+      const _ref = React.createRef(null);
+      refs[index] = _ref;
+      if (React.isValidElement(child)) {
+        return React.cloneElement(child, { ref: _ref });
+      } else {
+        return child;
+      }
+    });
+    useEffect(() => {
+      const htmlElements = refs
+        .map((_ref) => {
+          return ReactDOM.findDOMNode(_ref.current);
+        })
+        .filter((htmlElement) => htmlElement !== null);
+  
+      const removeEventListeners = htmlElements.map((el) => {
+        return addEventListener(el);
+      });
+      return () => {
+        removeEventListeners.forEach((fn) => fn());
+      };
+    });
+
+    return _children;
+  };
+  
+  export default EventShell
